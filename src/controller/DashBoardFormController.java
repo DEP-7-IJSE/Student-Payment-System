79927880c2ebf3c4d31e8aea7baee1924d417115
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-06-02 20:26:20 +0530

    Added Side Menu

--- src/controller/DashBoardFormController.java
+++ src/controller/DashBoardFormController.java
@@ -1,7 +1,14 @@
 package controller;
 
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class DashBoardFormController {
     public AnchorPane dashBoard;
@@ -9,8 +16,27 @@ public class DashBoardFormController {
     public TableColumn registrationCol;
     public TableColumn paymentCol;
     public TableColumn incomeCol;
+    public JFXHamburger btnMenu;
+    public JFXDrawer dwrSideMenu;
 
     public void initialize(){
+        try {
+            Parent menuPane = FXMLLoader.load(this.getClass().getResource("/view/SideMenu.fxml"));
+            dwrSideMenu.setSidePane(menuPane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        HamburgerBasicCloseTransition bt = new HamburgerBasicCloseTransition(btnMenu);
+        bt.setRate(-1);
+        btnMenu.setOnMouseClicked(e ->{
+            bt.setRate(bt.getRate()*-1);
+            bt.play();
+            if(dwrSideMenu.isClosed()){
+                dwrSideMenu.open();
+            }else{
+                dwrSideMenu.close();
+            }
+        });
     }
 }
