7dd1dfb72513c92e61e69f351e25829d47bc8b8e
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-06-05 00:13:03 +0530

    Modified NavBar

--- src/controller/PaymentFormController.java
+++ src/controller/PaymentFormController.java
@@ -2,16 +2,23 @@ package controller;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class PaymentFormController {
 
     public ImageView imgBack;
+    public Label lblDate;
+
+    public void initialize(){
+        lblDate.setText(String.valueOf(LocalDate.now()));
+    }
 
     public void imgBackClicked(MouseEvent mouseEvent) throws IOException {
         Stage back = (Stage) imgBack.getScene().getWindow();
