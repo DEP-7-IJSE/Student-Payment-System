ca7d3d3e148f2f808f621ca1e4fdb434dc39613b
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-06-05 01:48:24 +0530

    remove timeline

--- src/controller/SideMenuController.java
+++ src/controller/SideMenuController.java
@@ -1,8 +1,15 @@
 package controller;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
