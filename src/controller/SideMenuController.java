5f2197b0a2dca4082d6f41d67820b71038e770b4
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-06-05 22:31:56 +0530

    Added animation for payment.fxml

--- src/controller/SideMenuController.java
+++ src/controller/SideMenuController.java
@@ -1,12 +1,22 @@
 package controller;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class SideMenuController {
+
+    public StackPane sideMenuContainer;
     public AnchorPane sideMenuPane;
 
     public void initialize() {
@@ -18,7 +28,21 @@ public class SideMenuController {
     }
 
     public void managePaymentClicked(MouseEvent mouseEvent) {
-        navigateMenuItem("ManagePaymentForm.fxml");
+        try {
+            Parent root = FXMLLoader.load(this.getClass().getResource("../view/menu/ManagePaymentForm.fxml"));
+            Scene scene = sideMenuContainer.getScene();
+            root.translateYProperty().set(scene.getHeight());
+            sideMenuContainer.getChildren().add(root);
+            Timeline timeline= new Timeline();
+            KeyValue kv = new KeyValue(root.translateYProperty(),0, Interpolator.EASE_IN);
+            KeyFrame kf=new KeyFrame(Duration.seconds(1),kv);
+            timeline.getKeyFrames().add(kf);
+            timeline.setOnFinished(event -> sideMenuContainer.getChildren().remove(sideMenuPane));
+            timeline.play();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //navigateMenuItem("ManagePaymentForm.fxml");
     }
 
     public void manageUsersClicked(MouseEvent mouseEvent) {
