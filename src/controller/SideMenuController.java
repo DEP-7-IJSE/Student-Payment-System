c65d2738ad588f3a80187edffc87db90b59cadaf
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-06-07 22:59:16 +0530

    lblManageCourses colored

--- src/controller/SideMenuController.java
+++ src/controller/SideMenuController.java
@@ -7,9 +7,15 @@ import javafx.animation.Timeline;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 import java.io.IOException;
@@ -18,9 +24,14 @@ public class SideMenuController {
 
     public StackPane sideMenuContainer;
     public AnchorPane sideMenuPane;
+    public Label lblManageStudent;
+    public Label lblManageCourse;
+    public Label lblManagePayment;
+    public Label lblManageUsers;
 
     public void initialize() {
         manageCourseClicked(null);
+        changeColors();
     }
 
     public void manageCourseClicked(MouseEvent mouseEvent){
@@ -58,5 +69,17 @@ public class SideMenuController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void changeColors(){
+        /*Canvas canvas = new Canvas(lblManageStudent.getPrefWidth(), lblManageStudent.getPrefHeight());
+        GraphicsContext ctx = canvas.getGraphicsContext2D();
+        lblManageStudent.*/
+        lblManageStudent.setOnMouseClicked(event -> {
+            lblManageCourse.setBackground(new Background(new BackgroundFill(Color.valueOf("#7f8c8d"),null,null)));
+            lblManageUsers.setBackground(new Background(new BackgroundFill(Color.valueOf("#7f8c8d"),null,null)));
+            lblManagePayment.setBackground(new Background(new BackgroundFill(Color.valueOf("#7f8c8d"),null,null)));
+            lblManageStudent.setBackground(new Background(new BackgroundFill(Color.valueOf("#576574"),null,null)));
+        });
     }
 }
