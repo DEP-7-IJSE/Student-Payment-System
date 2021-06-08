17abe30df4ae9823355d77e8a18359b63c38592c
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-06-09 01:06:43 +0530

    Added month to UI

--- src/controller/menu/GetReportController.java
+++ src/controller/menu/GetReportController.java
@@ -1,4 +1,16 @@
 package controller.menu;
 
+import javafx.scene.control.Label;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class GetReportController {
+    public Label lblMonth;
+
+    public void initialize(){
+        Date month = new Date();
+        String format = new SimpleDateFormat("yyyy-MMMM").format(month);
+        lblMonth.setText(format);
+    }
 }
